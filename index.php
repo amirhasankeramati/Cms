a59d9e6b3aad0ba46f5e0ef7b82394e831e214af
amirhasan <amirh1749@gmail.com>
2016-11-14 22:41:55 +0330

    add index

---
+++ index.php
@@ -0,0 +1,68 @@
+<!Doctype html>
+<html>
+<head>
+<title>وب سایت شخصی</title>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+</head>
+<body>
+
+<div id="box-setting">
+<div id="right">
+
+<div class="right-box">
+<?php include("menus/menu.html"); ?>
+</div>
+<br>
+<div class="right-box" style="color:black;text-align:left;">
+<?php include("menus/project.html"); ?>
+</div>
+
+
+</div>
+
+<div id="left">
+
+<div class="left-box">
+<div class="title"><i class="fa fa-edit" style="font-size:38px;color:green"></i> سایت شخصی من</div>
+<div class="title-post">عنوان مطلب </div>
+<div class="text-post">
+<div class="timedate">
+<i class="fa fa-calendar" style="font-size:18px;color:red"></i><i class="time">1395/08/12</i>
+<i class="fa fa-clock-o" style="color:green;font-size:18px"> </i><i class="time">14:00</i></div>
+<div class="both"></div>
+<div class="text-post">
+این یک متن امتحانی است.
+</div>
+</div>
+<hr class="hrclass" style="color:#f5f5f0">
+
+<div class="title-post">عنوان مطلب </div>
+<div class="text-post">
+<div class="timedate">
+<i class="fa fa-calendar" style="font-size:18px;color:red"></i><i class="time">1395/08/12</i>
+<i class="fa fa-clock-o" style="color:green;font-size:18px"> </i><i class="time">14:00</i></div>
+<div class="both"></div>
+<div class="text-post">
+این یک متن امتحانی است.
+</div>
+</div>
+
+</div>
+<br>
+<div class="left-box">
+<div class="title"><i class="fa fa-camera" style="font-size:38px;color:blue"></i> آلبوم تصاویر</div>
+
+
+
+
+
+</div>
+
+
+</div>
+</div>
+
+
+</body>
