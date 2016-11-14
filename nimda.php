c3ff7a33aee55ece110589d0f653c661df00982d
amirhasan <amirh1749@gmail.com>
2016-11-14 22:42:36 +0330

    add admin login

---
+++ nimda.php
@@ -0,0 +1,61 @@
+<?php
+include("class.php");
+$connect=new Login();
+$connect->nimda();
+?>
+<!Doctype html>
+<html>
+<head>
+<title>وب سایت شخصی</title>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+</head>
+<body>
+
+<div id="box-setting">
+<div id="right">
+
+<div class="right-box">
+<?php include("menus/menu.html"); ?>
+</div>
+<br>
+<div class="right-box" style="color:black;text-align:left;">
+<?php include("menus/project.html"); ?>
+</div>
+
+
+</div>
+
+<div id="left">
+
+<div class="left-box">
+<?php
+if(isset($_POST['username']))
+{
+	$user=$_POST['username'];
+	$pass=$_POST['pass'];
+	$connect->setLogin($user,$pass);
+	$connect->getLogin();
+}
+?>
+
+
+<div class="title"><i class="fa fa-key" style="font-size:38px;color:green"></i> ورود به پنل</div>
+<form action="" method="post">
+<input type="text" name="username" id="username" placeholder="نام کاربری">
+<input type="password" name="pass" id="pass" placeholder="پسورد">
+<input type="submit" name="submit" value="ورود">
+
+
+</form>
+
+</div>
+
+
+
+</div>
+</div>
+
+
+</body>
