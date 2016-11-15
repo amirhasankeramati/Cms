6b9a22da93f70da740d9ac29340482a3d29dcbe1
amirhasan <amirh1749@gmail.com>
2016-11-15 23:18:25 +0330

    1- edit php files... 2- add errors and action 3- edit file open 4- fix bugs

--- panel.php
+++ panel.php
@@ -6,32 +6,17 @@ if(isset($_GET['namepage']))
 {
 $page=$_GET['namepage'];
 $connect->page($page);
-$namePage=$connect->namePage;
-	   $filer = fopen( "$namePage", "r" ) or header ("location: panel.php") ;
-
 }
 if(isset($_GET['out']))
 {
 	$connect->logout();
-	header ("location: index.html");
 }
 if(isset($_POST['edit']))
 {
-	if($_POST['security'] == "44651812")
-	{
-		
-	
-	$filew = fopen( "$namePage", "w+" ) or exit ( "Unable to open file!" ) ;
-	$edit=$_POST['text'];
-   fwrite($filew,$edit);
-    fclose( $filew ) ;
-	}
-	else
-	{
-		echo "<script>alert('خطا !');</script>";
-	}
+	$connect->editpage($page);
 }
 ?>
+
 <!Doctype html>
 <html>
 <head>
@@ -64,18 +49,27 @@ if(isset($_POST['edit']))
 <div class="title"><i class="fa fa-key" style="font-size:38px;color:green"></i>پنل مدیریت
  - <?php echo $_SESSION['username']; ?>  - <a href="panel.php?out=">خروج</a> 
  <br>
-
+<div class="error-class danger" id="errors"></div>
+<div class="error-class success" id="success"></div>
+<div class="error-class info" id="info"></div>
+<?php
+if(isset($_GET['action']))
+{
+	$action=$_GET['action'];
+$connect->actions($action);
+}
+?>
  <?php
  if(isset($_GET['namepage']))
  {
-	 echo "<div style='text-align:left'>". $namePage ."</div>";
+	 echo "<div style='text-align:left'>". $connect->namePage ."</div>";
   echo "<form action='' method='post'><textarea rows='50' cols='94' style='text-align:left;direction:ltr' id='text' name='text'>";
-  while ( !feof ( $filer ) )
+  while ( !feof ( $connect->filer ) )
     {
-      echo fgets ( $filer );
+      echo fgets ( $connect->filer );
     }
 	echo "</textarea><input type='text' name='security' id='security' placeholder='کلمه امنیتی جهت ویرایش فایل را وارد کنید.'><input type='submit' name='edit' id='edit' value='ویرایش'></form>";
-  fclose( $filer ) ;
+  fclose( $connect->filer ) ;
  }
  ?>
 </div>
