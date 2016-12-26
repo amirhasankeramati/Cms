3fc71f17fb91b9f898e1a8e84279f67b5238a9b2
amirhasan <amirh1749@gmail.com>
2016-12-26 20:36:51 +0330

    add config

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php 
+if($_SERVER['REQUEST_URI'] == '/me/config.php'){header("location: 404.html");}
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) exit('No direct access allowed.');
+// Start name pages ...
+		define('TITLE' , 'وب سایت شخصی');
+		define('NAME','امیرحسن');
+		define('FAMILY','کرامتی');
+		define('COUNTRY','ایران');
+		define('BIRTHDAY','73/12/28');
+		define('EMAIL','amirh1749@gmail.com');
+		define('ALBUM','آلبوم تصاویر');
+		define('STYLE','/me/style.css');
+		define('TITLELOGIN','ورود به پنل');
+		define('TABLEPOST','posts');
+//End name pages ...
+?>
