5bb0b399af3781e8e9223fd0f85debafbfba3da2
amirhasan <amirh1749@gmail.com>
2017-01-15 03:05:56 -0800

    edit config

--- config.php
+++ config.php
@@ -2,6 +2,7 @@
 if($_SERVER['REQUEST_URI'] == '/me/config.php'){header("location: 404.html");}
 if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) exit('No direct access allowed.');
 // Start name pages ...
+			
 		define('TITLE' , 'وب سایت شخصی');
 		define('NAME','امیرحسن');
 		define('FAMILY','کرامتی');
@@ -10,6 +11,8 @@ if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) exit('No direct access allowed.');
 		define('EMAIL','amirh1749@gmail.com');
 		define('ALBUM','آلبوم تصاویر');
 		define('STYLE','/me/style.css');
+		define('SCRIPT','/me/script/script.js');
+		define('JQUERY','/me/script/jquery.js');
 		define('TITLELOGIN','ورود به پنل');
 		define('TABLEPOST','posts');
 //End name pages ...
